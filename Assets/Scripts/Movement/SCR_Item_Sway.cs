90fcabe64a849d85c3079832098f98dc878ae8b2
gittne <gustav.eriksson6@gmail.com>
2023-12-09 01:20:53 +0100

    asdasdsadasddsa

--- Assets/Scripts/Movement/SCR_Item_Sway.cs
+++ Assets/Scripts/Movement/SCR_Item_Sway.cs
@@ -22,26 +22,6 @@ public class SCR_Item_Sway : NetworkBehaviour
     [SerializeField] float xMaxMovement; //The max distance the item can travel on the X axis to the right
     [SerializeField] float yMinMovement; //The max distance the item can travel on the Y axis to the left
     [SerializeField] float yMaxMovement; //The max distance the item can travel on the Y axis to the right
-    [Header("Bobbing Values")]
-    [SerializeField] float curveSpeed;
-    [SerializeField] Vector3 travelLimit = Vector3.one * 0.025f;
-    [SerializeField] Vector3 bobLimit = Vector3.one * 0.01f;
-
-    [SerializeField] float bobSmoothing = 10f;
-    [SerializeField] float smoothingRotation = 12f;
-
-    float sinCurve { get => Mathf.Sin(curveSpeed); }
-    float cosCurve { get => Mathf.Cos(curveSpeed); }
-
-    Vector3 bobPosition;
-
-    float horizontalInput;
-    float verticalInput;
-    Vector2 horizontalVerticalInput;
-
-    [Header("Bobbing Rotation Values")]
-    [SerializeField] Vector3 multiplier;
-    Vector3 eulerRotation;
 
     void Awake()
     {
@@ -59,17 +39,7 @@ public class SCR_Item_Sway : NetworkBehaviour
             return;
         }
 
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-
-        horizontalVerticalInput.x = horizontalInput;
-        horizontalVerticalInput.y = verticalInput;
-
         Sway();
-
-        BobOffset();
-        BobRotation();
-        //CompositePositionRotation();
     }
 
     //The method which is responsible for the rotation and movement of the arm when moving the mouse
@@ -92,7 +62,7 @@ public class SCR_Item_Sway : NetworkBehaviour
         targetRotation.y = Mathf.Clamp(targetRotation.y, -yMaxRotation, yMaxRotation);
 
         //The code which rotates the item
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, bobSmoothing * Time.deltaTime);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, swaySmoothing * Time.deltaTime);
 
         // These floats take the input from the mouse when moving it
         float moveX = Input.GetAxisRaw("Mouse X");
@@ -110,36 +80,5 @@ public class SCR_Item_Sway : NetworkBehaviour
 
         // Apply damping to the movement
         transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, lerpSpeed * Time.deltaTime);
-    }
-
-    void BobRotation()
-    {
-        eulerRotation.x = (horizontalVerticalInput != Vector2.zero ?
-            multiplier.x * (Mathf.Sin(2 * curveSpeed)) : multiplier.x * (Mathf.Sin(2 * curveSpeed)) / 2);
-
-        eulerRotation.y = (horizontalVerticalInput != Vector2.zero ? multiplier.y * cosCurve : 0);
-
-        eulerRotation.z = (horizontalVerticalInput != Vector2.zero ? multiplier.z * cosCurve * horizontalVerticalInput.x : 0);
-    }
-
-    void BobOffset()
-    {
-        curveSpeed += Time.deltaTime * (controller.isGrounded ? controller.velocity.magnitude : 1f) + 0.01f;
-
-        bobPosition.x = (cosCurve * bobLimit.x * (controller.isGrounded ? 1 : 0))
-            - (horizontalVerticalInput.x * travelLimit.x);
-
-        bobPosition.y = (sinCurve * bobLimit.y)
-            - (controller.velocity.y * travelLimit.y);
-
-        bobPosition.z = -(horizontalVerticalInput.y * travelLimit.z);
-    }
-
-    void CompositePositionRotation()
-    {
-
-        transform.localPosition = Vector3.Lerp(transform.localPosition, bobPosition, Time.deltaTime * bobSmoothing);
-
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(eulerRotation), Time.deltaTime * smoothingRotation);
     }
 }
