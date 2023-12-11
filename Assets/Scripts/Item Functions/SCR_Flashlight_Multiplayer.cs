c0f008a81c267f93ccd5ff8914bffd24c7e58f4b
gittne <gustav.eriksson6@gmail.com>
2023-12-11 16:07:49 +0100

    Added new multiplayer script to flashlight

---
+++ Assets/Scripts/Item Functions/SCR_Flashlight_Multiplayer.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class SCR_Flashlight_Multiplayer : NetworkBehaviour
+{
+    [Header("Light Sources")]
+    [SerializeField] Light spotLight;
+    [SerializeField] Light lightBulb;
+
+    [Header("Audio")]
+    [SerializeField] AudioClip onSound;
+    [SerializeField] AudioClip offSound;
+    [SerializeField] AudioSource audioSource;
+
+    bool isEnabled;
+
+    public NetworkVariable<bool> State = new NetworkVariable<bool>();
+
+    void Start()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        spotLight.enabled = false;
+        lightBulb.enabled = false;
+        isEnabled = false;
+    }
+
+    void Update()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ToggleFlashlightServerRpc(isEnabled);
+        }
+    }
+
+    public void ChangeFlashlightState(bool currentState)
+    {
+        if (State.Value)
+        {
+            audioSource.PlayOneShot(onSound);
+            spotLight.enabled = true;
+            lightBulb.enabled = true;
+        }
+        else
+        {
+            audioSource.PlayOneShot(offSound);
+            spotLight.enabled = false;
+            lightBulb.enabled = false;
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void ToggleFlashlightServerRpc(bool enabled, ServerRpcParams serverRpcParams = default)
+    {
+        var clientId = serverRpcParams.Receive.SenderClientId;
+        if (NetworkManager.ConnectedClients.ContainsKey(clientId))
+        {
+            ChangeFlashlightState(enabled);
+
+            isEnabled = !isEnabled;
+        }
+    }
+}
