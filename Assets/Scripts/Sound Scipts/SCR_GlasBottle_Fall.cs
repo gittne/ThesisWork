71789cd907bfaf0df3e4d304f343c9cc470c995e
bensimonschelhaas <98400638+bensimonschelhaas@users.noreply.github.com>
2023-12-29 18:04:21 +0100

    glass bottle script

---
+++ Assets/Scripts/Sound Scipts/SCR_GlasBottle_Fall.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_GlasBottle_Fall : MonoBehaviour
+{
+    [SerializeField] AudioSource soundSource;
+    [SerializeField] GameObject glassbottle;
+    // Start is called before the first frame update
+  
+
+     private void OnTriggerEnter(Collider other)
+    {
+        soundSource.Play();
+        Destroy(glassbottle,2f);
+    }
+}
