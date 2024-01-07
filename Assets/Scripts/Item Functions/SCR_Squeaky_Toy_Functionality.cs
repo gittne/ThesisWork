2b1cf2e1a45a346a62e981c05a17c6b89677c1b9
gittne <gustav.eriksson6@gmail.com>
2024-01-07 19:20:39 +0100

    Added throwing animation for toy and fixed fuse prefabs

--- Assets/Scripts/Item Functions/SCR_Squeaky_Toy_Functionality.cs
+++ Assets/Scripts/Item Functions/SCR_Squeaky_Toy_Functionality.cs
@@ -9,34 +9,37 @@ public class SCR_Squeaky_Toy_Functionality : MonoBehaviour
     [SerializeField] GameObject toyVisualObject;
     [SerializeField] GameObject itemHolder;
     [SerializeField] CharacterController controller;
+    [SerializeField] Animator animator;
+    [SerializeField] string animationTrigger;
     public bool isHolding { get; private set; }
     [Header("Throwing Variables")]
     [SerializeField] float timeToWindUpThrow;
+    [SerializeField] float timeToBringArmDown;
     [SerializeField] float initialVelocity;
     [SerializeField] float initialRotationVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire2"))
+        if (Input.GetButtonDown("Fire2") && isHolding)
         {
+            animator.enabled = true;
             StartCoroutine(ThrowingToy());
         }
 
-        if (isHolding)
+        if (!isHolding)
         {
-            toyVisualObject.SetActive(true);
+            itemHolder.transform.localPosition = Vector3.Lerp(itemHolder.transform.localPosition, new Vector3(0f, 0f, 0.3f), 3f * Time.deltaTime);
         }
         else
         {
-            toyVisualObject.SetActive(false);
-            itemHolder.transform.localPosition = Vector3.Lerp(itemHolder.transform.localPosition, new Vector3(0f, 0f, 0.3f), 3f * Time.deltaTime);
+            toyVisualObject.SetActive(true);
         }
     }
 
@@ -49,9 +52,11 @@ public class SCR_Squeaky_Toy_Functionality : MonoBehaviour
     {
         if (isHolding)
         {
-            //Play animation of throwing toy
+            animator.Play(animationTrigger, 0, 0f);
 
             yield return new WaitForSeconds(timeToWindUpThrow);
+
+            toyVisualObject.SetActive(false);
 
             GameObject instantiatedObject = Instantiate(toyPrefab, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), new Quaternion(spawnPoint.rotation.x, 0f, 0f, 0f));
 
@@ -60,9 +65,16 @@ public class SCR_Squeaky_Toy_Functionality : MonoBehaviour
             Vector3 randomRotation = new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
 
             rigidbody.AddForce(spawnPoint.forward * initialVelocity, ForceMode.Impulse);
+
             rigidbody.angularVelocity = randomRotation * initialRotationVelocity;
 
             isHolding = false;
+
+            yield return new WaitForSeconds(timeToBringArmDown);
+
+            animator.enabled = false;
+
+            itemHolder.transform.localPosition = Vector3.Lerp(itemHolder.transform.localPosition, new Vector3(0f, 0f, 0.3f), 3f * Time.deltaTime);
         }
     }
 }
