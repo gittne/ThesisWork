4b219c2fabb6329287b37bfbbb4e65395bbed79d
Tiger <tigominecraft@gmail.com>
2024-01-08 21:10:26 +0100

    multiplayer death almost work

--- Assets/Scripts/SCR_EnemyKill.cs
+++ Assets/Scripts/SCR_EnemyKill.cs
@@ -39,8 +39,6 @@ public class SCR_EnemyKill_Multiplayer : MonoBehaviour
         if (SCR_MultiplayerOverlord.Instance != null)
         {
             Debug.Log("multiplayer kill.");
-            //SCR_MultiplayerOverlord.Instance.KillAllPlayersServerRpc();
-
             player.GetComponent<SCR_First_Person_Controller>().CommencePlayerDeath(player);
             StartCoroutine(KillPlayerCutScene(true));
         }
