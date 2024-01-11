b8579f3835de6d8c58dbd5eddcccdc7d760693ae
gittne <gustav.eriksson6@gmail.com>
2024-01-11 02:10:27 +0100

    Test tedt 123

--- Assets/Scripts/Item Functions/Inventory/SCR_Inventory_Pickup_Multiplayer.cs
+++ Assets/Scripts/Item Functions/Inventory/SCR_Inventory_Pickup_Multiplayer.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class SCR_Inventory_Pickup_Multiplayer : MonoBehaviour
+public class SCR_Inventory_Pickup_Multiplayer : NetworkBehaviour
 {
     [SerializeField] SCR_Inventory_Item_Data referenceItem;
 
