f2bf7e185536b7fc502a42c2df1c59be0f763a99
Alex P <48757207+LiterallyAlex@users.noreply.github.com>
2024-01-19 13:07:53 +0100

    Modiofied LisaAlex menu

---
+++ Assets/Personal Folders/Davinchi/ForsningStuff/SCR_Menu_Alex.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SCR_Menu_Alex : MonoBehaviour
+{
+    int SceneID;
+    [SerializeField] GameObject controlscreen;
+    [SerializeField] GameObject Introcut;
+    [SerializeField] GameObject Menu;
+
+    private void Awake()
+    {
+        controlscreen.transform.localScale = new Vector3(0, 0, 0);
+        Introcut.transform.localScale = new Vector3(0, 0, 0);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void BlueScene()
+    {
+        SceneID = 1;
+        Introcut.transform.localScale = new Vector3(1, 1, 1);
+    } 
+    public void RedScene()
+    {
+        SceneID = 2;
+        Introcut.transform.localScale = new Vector3(1, 1, 1);
+    }
+    public void GreenScene()
+    {
+        SceneID = 3;
+        Introcut.transform.localScale = new Vector3(1,1,1);
+    }
+    
+
+    public void Controls()
+    {
+        Menu.transform.localScale = new Vector3(0, 0, 0);
+        controlscreen.transform.localScale = new Vector3(1, 1, 1);
+    }
+    public void MenuLoad()
+    {
+        Menu.transform.localScale = new Vector3(1, 1, 1);
+        controlscreen.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    public void SceneLoader()
+    {
+        SceneManager.LoadScene(SceneID);
+    }
+
+}
---
+++ Assets/Personal Folders/Davinchi/ForsningStuff/SCR_WinScreen.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_WinScreen : MonoBehaviour
+{
+    SCR_Key_Card_Reader KeyReader;
+
+
+    private void Update()
+    {
+        
+    }
+
+}
