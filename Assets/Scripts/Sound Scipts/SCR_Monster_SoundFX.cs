0aef880971dbf1ff4e896b7d0909f884b0a1dff7
bensimonschelhaas <98400638+bensimonschelhaas@users.noreply.github.com>
2024-01-22 18:34:35 +0100

    monster sound fx script

--- Assets/Scripts/Sound Scipts/SCR_Monster_SoundFX.cs
+++ Assets/Scripts/Sound Scipts/SCR_Monster_SoundFX.cs
@@ -9,38 +9,87 @@ public class SCR_Monster_SoundFX : MonoBehaviour
     [SerializeField] AudioClip[] IdleSounds;
     [SerializeField] AudioSource volSounds;
     [SerializeField] AudioClip[] SpottedSounds;
-    [SerializeField] AudioClip[] TriggerdSounds;
     [SerializeField] AudioClip[] AngrySounds;
     [SerializeField] float waitInterwall;
     [SerializeField] float repInterwall;
     [SerializeField] float startRepeating;
-    private bool gameIsRunning = true;
+    bool canPlayNewSounds = true;
+    float Thold = 0;
 
-   
+
+    SCR_EnemyBrain brain;
+
     void Start()
     {
-        InvokeRepeating("IdleSoundS", startRepeating, repInterwall);
-
+        brain = GetComponent<SCR_EnemyBrain>();
+        Thold = UnityEngine.Random.Range(0, 55);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!canPlayNewSounds) return;
+        StartCoroutine(SoundDelay());
 
-    }
-
-     void IdleSoundS()
-    {
-        if (gameIsRunning == true)
+        if (brain.GetRageAmount() < 30)
         {
-            
-            float Thold = Random.Range(0, 101);
-            if (Thold > waitInterwall)
-            {
-                volSounds.PlayOneShot(IdleSounds[Random.Range(0, IdleSounds.Length - 1)]);
-            }
+            IdleSoundManager();
         }
-        
+        else if (brain.GetRageAmount() < 80 && brain.GetRageAmount() > 30)
+        {
+            FollowingSoundManager();
+
+        }
+        else if (brain.GetRageAmount() > 80)
+        {
+            AngrySoundManager();
+        }
     }
+
+    void IdleSoundManager()
+    {
+
+        if (Thold > waitInterwall)
+        {
+            volSounds.PlayOneShot(IdleSounds[Random.Range(0, IdleSounds.Length - 1)]);
+    
+        }
+
+    }
+
+    void FollowingSoundManager()
+    {
+
+        if (Thold > waitInterwall)
+        {
+            volSounds.PlayOneShot(SpottedSounds[Random.Range(0, SpottedSounds.Length - 1)]);
+           
+        }
+
+    }
+
+    void AngrySoundManager()
+    {
+
+        if (Thold > waitInterwall)
+        {
+            volSounds.PlayOneShot(AngrySounds[Random.Range(0, AngrySounds.Length - 1)]);
+           
+        }
+
+    }
+
+    IEnumerator SoundDelay()
+    {
+        canPlayNewSounds = false;
+        yield return new WaitForSeconds(5);
+        canPlayNewSounds = true;
+    }
+
+    void KillSound()
+    {
+
+    }
+
+
 }
