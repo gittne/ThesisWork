809d9e08582d094237ea30675d49af3f6ccb0a89
gittne <gustav.eriksson6@gmail.com>
2024-01-25 03:57:46 +0100

    Fixed shadowrendering for objects in vr backpack

--- Assets/Scripts/VR Scripts/SCR_XR_Socket_Interactor_Backpack.cs
+++ Assets/Scripts/VR Scripts/SCR_XR_Socket_Interactor_Backpack.cs
@@ -7,21 +7,44 @@ public class SCR_XR_Socket_Interactor_Backpack : XRSocketInteractor
 {
     private Vector3 originalScale;
     [SerializeField] float newScale;
+    Renderer objectRenderer;
 
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
         originalScale = args.interactableObject.transform.localScale;
+
+        objectRenderer = args.interactable.GetComponent<Renderer>();
+
+        if (objectRenderer != null)
+        {
+            SetShadows(false);
+        }
     }
 
     protected override void OnSelectExited(SelectExitEventArgs args)
     {
         base.OnSelectExited(args);
-        ScaleObject(args.interactable); 
+        ScaleObject(args.interactable);
+
+        if (objectRenderer != null)
+        {
+            SetShadows(true);
+
+            objectRenderer = null;
+        }
     }
 
     private void ScaleObject(XRBaseInteractable interactable)
     {
         interactable.transform.localScale = originalScale;
+    }
+
+    private void SetShadows(bool enableShadows)
+    {
+        if (objectRenderer != null)
+        {
+            objectRenderer.shadowCastingMode = enableShadows ? UnityEngine.Rendering.ShadowCastingMode.On : UnityEngine.Rendering.ShadowCastingMode.Off;
+        }
     }
 }
