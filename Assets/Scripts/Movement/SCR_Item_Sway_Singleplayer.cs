4ff8340f10c1216c1c6784c4c8c8505f60964588
gittne <gustav.eriksson6@gmail.com>
2024-01-29 13:49:59 +0100

    Adjusted item sway for flashlight and added new variable for item sway script

--- Assets/Scripts/Movement/SCR_Item_Sway_Singleplayer.cs
+++ Assets/Scripts/Movement/SCR_Item_Sway_Singleplayer.cs
@@ -18,6 +18,7 @@ public class SCR_Item_Sway_Singleplayer : MonoBehaviour
     [SerializeField] float swayMultiplier; //The multiplier for sway amount
     [SerializeField] float xMaxRotation; //The max rotation the object can rotate on the X axis
     [SerializeField] float yMaxRotation; //The max rotation the object can rotate on the Y axis
+    [SerializeField] float mouseInputMultiplier;
     [SerializeField] Quaternion inventoryRotation;
     Quaternion targetRotation;
     [Header("Sway Movement Variables")]
@@ -36,14 +37,14 @@ public class SCR_Item_Sway_Singleplayer : MonoBehaviour
         //These floats take the input from the mouse when moving it
         if (!isInLeftHand)
         {
-            mouseX = Input.GetAxisRaw("Mouse X");
+            mouseX = Input.GetAxisRaw("Mouse X") * mouseInputMultiplier;
         }
         else
         {
-            mouseX = -Input.GetAxisRaw("Mouse X");
+            mouseX = -Input.GetAxisRaw("Mouse X") * mouseInputMultiplier;
         }
         
-        mouseY = Input.GetAxisRaw("Mouse Y");
+        mouseY = Input.GetAxisRaw("Mouse Y") * mouseInputMultiplier;
 
         Sway();
     }
