7b3bc22ac3c6d854eb7a0205322a50b64e3a515b
gittne <gustav.eriksson6@gmail.com>
2024-01-29 15:50:31 +0100

    Submbission 2 for pickuop

--- Assets/Scripts/Item Functions/Inventory/SCR_Inventory_Pickup_Multiplayer.cs
+++ Assets/Scripts/Item Functions/Inventory/SCR_Inventory_Pickup_Multiplayer.cs
@@ -6,10 +6,32 @@ using Unity.Netcode;
 public class SCR_Inventory_Pickup_Multiplayer : NetworkBehaviour
 {
     [SerializeField] SCR_Inventory_Item_Data referenceItem;
+    GameObject[] inventoryObjects;
+    SCR_Inventory_System_Multiplayer inventoryToAddTo;
+    float nearestDistance = 100000;
+
+    void Awake()
+    {
+        inventoryObjects = GameObject.FindGameObjectsWithTag("Inventory");
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < inventoryObjects.Length; i++)
+        {
+            float distance = Vector3.Distance(this.transform.position, inventoryObjects[i].transform.position);
+
+            if (distance < nearestDistance)
+            {
+                inventoryToAddTo = inventoryObjects[i].GetComponent<SCR_Inventory_System_Multiplayer>();
+                nearestDistance = distance;
+            }
+        }
+    }
 
     public void OnHandlePickupItem()
     {
-        SCR_Inventory_System_Multiplayer.current.AddItem(referenceItem);
+        inventoryToAddTo.AddItem(referenceItem);
         Destroy(gameObject);
     }
 }
