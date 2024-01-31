3070f7e05339cf45b43618b3a54e00598e616a81
gittne <gustav.eriksson6@gmail.com>
2024-01-31 19:55:11 +0100

    fghj

--- Assets/Scripts/Item Functions/Inventory/SCR_Inventory_Pickup_Multiplayer.cs
+++ Assets/Scripts/Item Functions/Inventory/SCR_Inventory_Pickup_Multiplayer.cs
@@ -6,10 +6,43 @@ using Unity.Netcode;
 public class SCR_Inventory_Pickup_Multiplayer : NetworkBehaviour
 {
     [SerializeField] SCR_Inventory_Item_Data referenceItem;
-    
+    float distance;
+    float nearestDistance = 1000000;
+    GameObject[] players;
+    GameObject nearestPlayer;
+    SCR_Inventory_System_Singleplayer nearestInventory;
+
+    private void Awake()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i] = GameObject.FindWithTag("Player");
+        }
+
+        Debug.Log("These are the player in the scene are: " + players);
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            distance = Vector3.Distance(this.transform.position, players[i].transform.position);
+
+            if (distance < nearestDistance)
+            {
+                nearestPlayer = players[i];
+                nearestDistance = distance;
+            }
+        }
+
+        nearestInventory = nearestPlayer.GetComponent<SCR_Inventory_System_Singleplayer>();
+
+        Debug.Log("The nearest player is: " + nearestPlayer + ", with the nearest inventory being: " + nearestInventory);
+    }
+
     public void OnHandlePickupItem()
     {
-        SCR_Inventory_System_Singleplayer.current.AddItem(referenceItem);
+        nearestInventory.AddItem(referenceItem);
         DestroyPickupServerRPC();
     }
 
