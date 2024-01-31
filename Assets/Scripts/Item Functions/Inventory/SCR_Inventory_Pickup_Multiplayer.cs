a65eefb07fde64164b77b44b9f3de8873b2d3225
gittne <gustav.eriksson6@gmail.com>
2024-01-31 20:04:33 +0100

    gdf

--- Assets/Scripts/Item Functions/Inventory/SCR_Inventory_Pickup_Multiplayer.cs
+++ Assets/Scripts/Item Functions/Inventory/SCR_Inventory_Pickup_Multiplayer.cs
@@ -6,25 +6,10 @@ using Unity.Netcode;
 public class SCR_Inventory_Pickup_Multiplayer : NetworkBehaviour
 {
     [SerializeField] SCR_Inventory_Item_Data referenceItem;
-    float distance;
-    float nearestDistance = 1000000;
-    GameObject[] players;
-    GameObject nearestPlayer;
-    SCR_Inventory_System_Singleplayer nearestInventory;
-
-    private void Awake()
-    {
-        MeowServerRPC();
-    }
-
-    private void Update()
-    {
-        DistanceCheckerServerRPC();
-    }
 
     public void OnHandlePickupItem()
     {
-        nearestInventory.AddItem(referenceItem);
+        SCR_Inventory_System_Singleplayer.current.AddItem(referenceItem);
         DestroyPickupServerRPC();
     }
 
@@ -32,35 +17,5 @@ public class SCR_Inventory_Pickup_Multiplayer : NetworkBehaviour
     void DestroyPickupServerRPC()
     {
         Destroy(gameObject);
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    void DistanceCheckerServerRPC()
-    {
-        for (int i = 0; i < players.Length; i++)
-        {
-            distance = Vector3.Distance(this.transform.position, players[i].transform.position);
-
-            if (distance < nearestDistance)
-            {
-                nearestPlayer = players[i];
-                nearestDistance = distance;
-            }
-        }
-
-        nearestInventory = nearestPlayer.GetComponent<SCR_Inventory_System_Singleplayer>();
-
-        Debug.Log("The nearest player is: " + nearestPlayer + ", with the nearest inventory being: " + nearestInventory);
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    void MeowServerRPC()
-    {
-        for (int i = 0; i < players.Length; i++)
-        {
-            players[i] = GameObject.FindWithTag("Player");
-        }
-
-        Debug.Log("These are the player in the scene are: " + players);
     }
 }
