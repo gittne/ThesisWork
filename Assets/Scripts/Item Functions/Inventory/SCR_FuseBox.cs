021dfdd6a5f50b5deb12f2c48199f3d97e48bb7e
gittne <gustav.eriksson6@gmail.com>
2024-05-22 03:50:25 +0200

    Added lamp activation and sounds for inserting fuses

--- Assets/Scripts/Item Functions/Inventory/SCR_FuseBox.cs
+++ Assets/Scripts/Item Functions/Inventory/SCR_FuseBox.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Netcode;
+//using Unity.Netcode;
 using UnityEngine;
 
 [RequireComponent(typeof(SCR_Light_Indicator))]
-public class SCR_FuseBox : NetworkBehaviour
+public class SCR_FuseBox : MonoBehaviour
 {
     public int fusesInserted { get; private set; }
     public bool canInsertFuse { get; private set; }
     public bool isActivated { get; private set; }
     [Range(1, 3)] [SerializeField] int fusesLeftToInsert;
     [SerializeField] GameObject[] fuseObjects;
+    [SerializeField] Light[] lights;
 
     void Start()
     {
@@ -43,11 +44,11 @@ public class SCR_FuseBox : NetworkBehaviour
     {
         Debug.Log("My fusebox was filled.");
 
-        if (SCR_MultiplayerOverlord.Instance != null)
-        {
-            FillFuseboxServerRpc();
-            return;
-        }
+        //if (SCR_MultiplayerOverlord.Instance != null)
+        //{
+        //    FillFuseboxServerRpc();
+        //    return;
+        //}
 
         fusesInserted++;
 
@@ -60,27 +61,35 @@ public class SCR_FuseBox : NetworkBehaviour
         {
             isActivated = true;
         }
-    }
 
-    [ServerRpc(RequireOwnership = false)]
-    public void FillFuseboxServerRpc()
-    {
-        FillFuseboxClientRpc();
-    }
-
-    [ClientRpc]
-    void FillFuseboxClientRpc()
-    {
-        fusesInserted++;
-
-        for (int i = 0; i < fusesInserted; i++)
+        if (isActivated && lights != null)
         {
-            fuseObjects[i].SetActive(true);
-        }
-
-        if (fusesInserted == fusesLeftToInsert)
-        {
-            isActivated = true;
+            foreach (Light light in lights)
+            {
+                light.enabled = true;
+            }
         }
     }
+
+    //[ServerRpc(RequireOwnership = false)]
+    //public void FillFuseboxServerRpc()
+    //{
+    //    FillFuseboxClientRpc();
+    //}
+
+    //[ClientRpc]
+    //void FillFuseboxClientRpc()
+    //{
+    //    fusesInserted++;
+
+    //    for (int i = 0; i < fusesInserted; i++)
+    //    {
+    //        fuseObjects[i].SetActive(true);
+    //    }
+
+    //    if (fusesInserted == fusesLeftToInsert)
+    //    {
+    //        isActivated = true;
+    //    }
+    //}
 }
